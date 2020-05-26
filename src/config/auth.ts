79fe2032c86bba0295ee2e841fcc3bf0db4399a0
gnf22 <gnferreira2000@gmail.com>
2020-05-26 06:59:01 -0300

    first commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '53988df8e2a3833375a9a446d256f4b2',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const CreateAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await CreateAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const CreateAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await CreateAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123',
+    });
+
+    expect(
+      CreateAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
